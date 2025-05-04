b459f1efb6be11faa65a925f0ef4d0b8a60a21b5
MOSTAKIM EDITZ <162802866+Mostakim0978@users.noreply.github.com>
2025-05-04 18:02:24 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 
-app.get("/x", async (req, res) => {
+app.get("/mostakim/x", async (req, res) => {
   const name = req.query.name;
 
   try {
